8da09764ec9dca18713cdb5f736870ace60d1c3e
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2014-07-19 09:36:29 -0700

    Submit coverage to coveralls in travis build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var jscs = require('gulp-jscs');
+var jscov = require('gulp-jscoverage');
 
-gulp.task('lint', function() {
+gulp.task('jscs', function() {
   return gulp.src([
-    'src/*.js',
+    'lib/*.js',
     'test/*.js'
   ]).pipe(jscs());
+});
+
+gulp.task('jscov', function() {
+  gulp.src('lib/*.js')
+    .pipe(jscov('index.js'))
+    .pipe(gulp.dest('./lib-cov'));
 });
