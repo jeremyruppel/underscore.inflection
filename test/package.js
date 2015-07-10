36806744b2df5cb31088e8b2ee5ca5c9f1725bb8
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2015-07-10 10:07:24 -0700

    Add tests for package parity

---
+++ test/package.js
@@ -0,0 +1,15 @@
+var package = require('../package');
+var bower = require('../bower');
+var assert = require('assert');
+
+describe('package', function () {
+  it('has the same package name', function () {
+    assert.equal(package.name, bower.name);
+  });
+  it('has the same package version', function () {
+    assert.equal(package.version, bower.version);
+  });
+  it('requires the same version of underscore', function () {
+    assert.equal(package.devDependencies.underscore, bower.dependencies.underscore);
+  });
+});
